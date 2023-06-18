f893fa416c8189eccb7d4fdb6305a8a30bad5fc7
MikhailV <vvlvvl24280@gmail.com>
2023-06-18 14:49:12 +0500

    correct

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:fx14ef@45.8.249.62:5432'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:fx14ef@45.8.249.62:5432/employees'
 
 db = SQLAlchemy(app)
 
